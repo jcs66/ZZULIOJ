17da13a08d3842855bfc30384fd1dd5303b384a4
Jincheng Song <114846817+jcs66@users.noreply.github.com>
2023-09-30 11:17:50 +0800

    1089 求阶乘最高位.cpp
    zzulioj

---
+++ 1089求阶乘最高位.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 21530 on 2023/9/29.
+//
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int return_maxdigit(int n) {
+    double result = 0;
+    for (int i = 1; i <= n; i++) {
+        result += log10(i);
+    }
+    int maxdigit = (int)pow(10,result-(int)result);
+    return maxdigit;
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int maxdigit = return_maxdigit(n);
+    printf("%d\n",maxdigit);
+    return 0;
+}
